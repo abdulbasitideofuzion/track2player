dc4aac9d0a2ab8d50099ab3de09f4aaa6e9e3676
abdulbasitideofuzion <abdul.basit@ideofuzion.com>
2021-08-23 10:47:23 +0500

    new changes

---
+++ android/src/main/java/com/track2player/TrackPlayer1.java
@@ -0,0 +1,32 @@
+package com.track2player;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import com.track2player.module.MusicModule2;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Track2Player
+ * https://github.com/react-native-kit/react-native-track-player
+ *
+ * @author Guichaguri
+ */
+public class TrackPlayer1 implements ReactPackage {
+
+
+
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    return Collections.singletonList(new MusicModule2(reactContext,"TrackPlayerModule1"));
+  }
+
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    return Collections.emptyList();
+  }
+
+}
